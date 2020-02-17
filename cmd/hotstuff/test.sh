1a72773ef3bf24bb367e77226d4ad6b1d4abd622
John Ingve Olsen <johningveolsen@gmail.com>
2020-02-17 12:06:03 +0100

    Add big file for testing

--- cmd/hotstuff/test.sh
+++ cmd/hotstuff/test.sh
@@ -9,17 +9,17 @@ if [[ "$1" == "debug" ]]; then
 	TIMEOUT="$((1000*10))"
 fi
 
-LEADER_ARG="--self-id 1 --keyfile keys/r1.key --leader-id 1 --commands main.go --timeout $TIMEOUT"
+LEADER_ARG="--self-id 1 --keyfile keys/r1.key --leader-id 1 --commands big.txt --timeout $TIMEOUT --cpuprofile cpuprofile.out"
 
-./hotstuff --self-id 2 --keyfile keys/r2.key --leader-id 1 --timeout $TIMEOUT &
-./hotstuff --self-id 3 --keyfile keys/r3.key --leader-id 1 --timeout $TIMEOUT &
-./hotstuff --self-id 4 --keyfile keys/r4.key --leader-id 1 --timeout $TIMEOUT &
+./hotstuff --self-id 2 --keyfile keys/r2.key --leader-id 1 --timeout $TIMEOUT >2.out &
+./hotstuff --self-id 3 --keyfile keys/r3.key --leader-id 1 --timeout $TIMEOUT >3.out &
+./hotstuff --self-id 4 --keyfile keys/r4.key --leader-id 1 --timeout $TIMEOUT >4.out &
 
 # start the leader last
 if [[ "$2" == "leader" ]]; then
 	dlv debug --headless --listen=:2345 --api-version=2 -- $LEADER_ARG
 else
-	./hotstuff $LEADER_ARG
+	./hotstuff $LEADER_ARG >1.out
 fi
 
-killall hotstuff
+killall hotstuff<
