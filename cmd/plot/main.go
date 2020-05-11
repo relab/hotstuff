9aa322e25409ac2e0b40a4a2e15e180cd5babd0b
John Ingve Olsen <johningveolsen@gmail.com>
2020-05-11 16:38:55 +0200

    cmd/plot: plot 95 confidence interval

--- cmd/plot/main.go
+++ cmd/plot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"image/color"
 	"io/ioutil"
@@ -19,15 +20,19 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
-type rawThroughput struct {
-	deltaTime   float64
-	numCommands float64
+type dataPoint struct {
+	deltaTime time.Duration
+	latency   time.Duration
 }
 
 type measurement struct {
-	rate       int
-	latency    float64
-	throughput float64
+	rate               int
+	latency            float64
+	latencyVariance    float64
+	throughput         float64
+	throughputVariance float64
+	numCommands        int
+	numThrMeasurements int
 }
 
 type benchmark struct {
@@ -39,9 +44,12 @@ type benchmark struct {
 
 func (b benchmark) String() string {
 	var ret strings.Builder
-	ret.WriteString(fmt.Sprintf("%s-b%d-p%d: ", b.name, b.batchSize, b.payloadSize))
+	ret.WriteString("------------------------------------------------------------\n")
+	ret.WriteString(fmt.Sprintf("%s: batch size b%d, payload size p%d\n", b.name, b.batchSize, b.payloadSize))
+	ret.WriteString(fmt.Sprintf("Throughput mean, Throughput Stdev, Latency mean, Latency Stdev\n"))
 	for _, m := range b.measurements {
-		ret.WriteString(fmt.Sprintf("(%f, %f), ", m.throughput, m.latency))
+		ret.WriteString(fmt.Sprintf("%.2f, %.2f, %.2f, %.2f\n",
+			m.throughput, math.Sqrt(m.throughputVariance), m.latency, math.Sqrt(m.latencyVariance)))
 	}
 	return ret.String()
 }
@@ -55,31 +63,48 @@ func (b *benchmark) XY(i int) (x, y float64) {
 	return m.throughput, m.latency
 }
 
+func (b *benchmark) XError(i int) (lower, upper float64) {
+	m := b.measurements[i]
+	lower = -1.96 * math.Sqrt(m.throughputVariance) / math.Sqrt(float64(m.numThrMeasurements))
+	if math.IsNaN(lower) {
+		lower = 0
+	}
+	upper = 1.96 * math.Sqrt(m.throughputVariance) / math.Sqrt(float64(m.numThrMeasurements))
+	if math.IsNaN(upper) {
+		upper = 0
+	}
+	return
+}
+
+func (b *benchmark) YError(i int) (lower, upper float64) {
+	m := b.measurements[i]
+	lower = -1.96 * math.Sqrt(m.latencyVariance) / math.Sqrt(float64(m.numCommands))
+	if math.IsNaN(lower) {
+		lower = 0
+	}
+	upper = 1.96 * math.Sqrt(m.latencyVariance) / math.Sqrt(float64(m.numCommands))
+	if math.IsNaN(upper) {
+		lower = 0
+	}
+	return
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [paths to benchmark files] [output file]\n", os.Args[0])
+	plotFile := flag.String("plot", "", "Destination to save plot in")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [paths to benchmark files]\n", os.Args[0])
 		os.Exit(1)
 	}
-
-	p, err := plot.New()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create plot: %v\n", err)
-		os.Exit(1)
-	}
-
-	grid := plotter.NewGrid()
-	grid.Horizontal.Color = color.Gray{Y: 200}
-	grid.Horizontal.Dashes = plotutil.Dashes(2)
-	grid.Vertical.Color = color.Gray{Y: 200}
-	grid.Vertical.Dashes = plotutil.Dashes(2)
-	p.Add(grid)
 
 	var plots []interface{}
+	var errorBars []interface{}
 	benchmarks := make(chan *benchmark, 1)
 	errors := make(chan error, 1)
 	n := 0
 
-	for _, benchFolder := range os.Args[1 : len(os.Args)-1] {
+	for _, benchFolder := range flag.Args() {
 		dir, err := ioutil.ReadDir(benchFolder)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to open benchmark directory: %v\n", err)
@@ -124,22 +149,56 @@ func main() {
 		plots[index+1] = b
 	}
 
-	err = plotutil.AddLinePoints(p, plots...)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to add plots: %v\n", err)
-		os.Exit(1)
+	for _, p := range plots {
+		if _, ok := p.(string); !ok {
+			errorBars = append(errorBars, p)
+		}
 	}
 
-	/* p.Legend.Left = true */
-	p.Legend.Top = true
-	p.X.Label.Text = "Throughput Kops/sec"
-	p.X.Tick.Marker = hplot.Ticks{N: 10}
-	p.Y.Label.Text = "Latency ms"
-	p.Y.Tick.Marker = hplot.Ticks{N: 10}
+	if *plotFile != "" {
+		p, err := plot.New()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create plot: %v\n", err)
+			os.Exit(1)
+		}
 
-	if err := p.Save(6*vg.Inch, 6*vg.Inch, os.Args[len(os.Args)-1]); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to save plot: %v\n", err)
-		os.Exit(1)
+		grid := plotter.NewGrid()
+		grid.Horizontal.Color = color.Gray{Y: 200}
+		grid.Horizontal.Dashes = plotutil.Dashes(2)
+		grid.Vertical.Color = color.Gray{Y: 200}
+		grid.Vertical.Dashes = plotutil.Dashes(2)
+		p.Add(grid)
+
+		err = plotutil.AddLinePoints(p, plots...)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to add plots: %v\n", err)
+			os.Exit(1)
+		}
+
+		err = plotutil.AddErrorBars(p, errorBars...)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to add error bars: %v\n", err)
+			os.Exit(1)
+		}
+
+		/* p.Legend.Left = true */
+		p.Legend.Top = true
+		p.X.Label.Text = "Throughput Kops/sec"
+		p.X.Tick.Marker = hplot.Ticks{N: 10}
+		p.Y.Label.Text = "Latency ms"
+		p.Y.Tick.Marker = hplot.Ticks{N: 10}
+
+		if err := p.Save(6*vg.Inch, 6*vg.Inch, *plotFile); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to save plot: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		// just print the benchmarks to stdout
+		for _, p := range plots {
+			if b, ok := p.(*benchmark); ok {
+				fmt.Println(b)
+			}
+		}
 	}
 }
 
@@ -165,20 +224,7 @@ func processBenchmark(dirPath string) (*benchmark, error) {
 	}
 	b.measurements = make([]measurement, 0, len(measurements))
 	for _, ms := range measurements {
-		latencyTotal, throughputTotal := 0.0, 0.0
-		rate := ms[0].rate
-		for _, m := range ms {
-			if m.rate != rate {
-				panic("Rate mismatch while combining measurements!")
-			}
-			latencyTotal += m.latency
-			throughputTotal += m.throughput
-		}
-		m := measurement{
-			rate:       rate,
-			latency:    latencyTotal / float64(len(ms)),
-			throughput: throughputTotal / float64(len(ms)),
-		}
+		m := combineMeasurements(false, ms...)
 		i := sort.Search(len(b.measurements), func(i int) bool {
 			return m.rate < b.measurements[i].rate
 		})
@@ -240,79 +286,146 @@ func processRun(dirPath string, measurements map[int][]measurement, benchType st
 }
 
 func readInMeasurement(dirPath string, nanoseconds bool) (measurement, error) {
+	dir, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return measurement{}, err
+	}
+
+	var data [][]dataPoint
+	for _, file := range dir {
+		if file.IsDir() {
+			dirPath := path.Join(dirPath, file.Name())
+			dir, err := ioutil.ReadDir(dirPath)
+			if err != nil {
+				return measurement{}, err
+			}
+			for _, file := range dir {
+				if file.IsDir() {
+					continue
+				}
+				d, err := readInData(path.Join(dirPath, file.Name()), nanoseconds)
+				if err != nil {
+					return measurement{}, err
+				}
+				data = append(data, d)
+			}
+			continue
+		}
+
+		d, err := readInData(path.Join(dirPath, file.Name()), nanoseconds)
+		if err != nil {
+			return measurement{}, err
+		}
+		data = append(data, d)
+	}
+
+	var totalLatency time.Duration
+	var throughput float64
+	numCommands := 0
+	for _, d := range data {
+		var totalTime time.Duration
+		numCommands += len(d)
+		for _, p := range d {
+			totalLatency += p.latency / time.Millisecond
+			totalTime += p.deltaTime
+		}
+		throughput += (float64(len(d)) / 1000) / totalTime.Seconds()
+	}
+	latencyMean := (float64(totalLatency) / float64(numCommands))
+	latencyVariance := 0.0
+	for _, d := range data {
+		for _, p := range d {
+			latencyVariance += math.Pow(float64(p.latency/time.Millisecond)-latencyMean, 2)
+		}
+	}
+	latencyVariance /= float64(numCommands - 1)
+
+	return measurement{
+		latency:            latencyMean,
+		latencyVariance:    latencyVariance,
+		throughput:         throughput,
+		numCommands:        len(data),
+		numThrMeasurements: 1,
+	}, nil
+}
+
+func readInData(filePath string, nanoseconds bool) ([]dataPoint, error) {
 	var numberFormat string
 	if nanoseconds {
 		numberFormat = "%sns"
 	} else {
 		numberFormat = "%sus"
 	}
-
-	dir, err := ioutil.ReadDir(dirPath)
+	f, err := os.Open(filePath)
 	if err != nil {
-		return measurement{}, err
+		return nil, err
 	}
-
-	var measurements []measurement
-	for _, file := range dir {
-		if file.IsDir() {
-			m, err := readInMeasurement(path.Join(dirPath, file.Name()), nanoseconds)
-			if err != nil {
-				return measurement{}, err
-			}
-			measurements = append(measurements, m)
+	var data []dataPoint
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		l := scanner.Text()
+		matches := strings.Split(l, " ")
+		if len(matches) < 2 {
 			continue
 		}
-
-		f, err := os.Open(path.Join(dirPath, file.Name()))
+		t, err := time.ParseDuration(fmt.Sprintf("%sns", matches[0]))
 		if err != nil {
-			return measurement{}, fmt.Errorf("Failed to read libhotstuff measurement: %w", err)
+			return nil, fmt.Errorf("Failed to read data: %w", err)
 		}
-
-		var totalLatency time.Duration
-		var totalTime time.Duration
-		numCommands := 0
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			l := scanner.Text()
-			matches := strings.Split(l, " ")
-			if len(matches) < 2 {
-				continue
-			}
-
-			t, err := time.ParseDuration(fmt.Sprintf("%sns", matches[0]))
-			if err != nil {
-				return measurement{}, fmt.Errorf("Failed to read libhotstuff measurement: %w", err)
-			}
-			lat, err := time.ParseDuration(fmt.Sprintf(numberFormat, matches[1]))
-			if err != nil {
-				return measurement{}, fmt.Errorf("Failed to read libhotstuff measurement: %w", err)
-			}
-			numCommands++
-			totalLatency += lat
-			totalTime += t
+		lat, err := time.ParseDuration(fmt.Sprintf(numberFormat, matches[1]))
+		if err != nil {
+			return nil, fmt.Errorf("Failed to read data: %w", err)
 		}
-		latency := (float64(totalLatency) / float64(numCommands)) / float64(time.Millisecond)
-		throughput := (float64(numCommands) / 1000) / totalTime.Seconds()
-		measurements = append(measurements, measurement{latency: latency, throughput: throughput})
+		data = append(data, dataPoint{deltaTime: t, latency: lat})
 	}
+	return data, nil
+}
 
+func combineMeasurements(sumThroughput bool, measurements ...measurement) measurement {
 	if len(measurements) == 0 {
-		return measurement{}, fmt.Errorf("No measurements")
+		panic("combineMeasurements called with no measurements!")
 	}
 
-	sumLatencies := 0.0
-	sumThroughput := 0.0
-	for _, m := range measurements {
-		if math.IsNaN(m.throughput) || math.IsNaN(m.latency) {
-			fmt.Fprintf(os.Stderr, "WARNING: Ignoring NaN measurement in '%s'\n", dirPath)
-			continue
-		}
-		sumLatencies += m.latency
-		sumThroughput += m.throughput
+	if len(measurements) == 1 {
+		return measurements[0]
 	}
+
+	rate := measurements[0].rate
+
+	// first need to calculate mean
+	latencyMean := 0.0
+	throughput := 0.0
+	numCommands := 0
+	numThrMeasurements := 0
+	for _, m := range measurements {
+		if m.rate != rate {
+			panic("Attempt to combine measurements with different rate.")
+		}
+		latencyMean += float64(m.numCommands) * m.latency
+		throughput += float64(m.numThrMeasurements) * m.throughput
+		numCommands += m.numCommands
+		numThrMeasurements += m.numThrMeasurements
+	}
+	latencyMean /= float64(numCommands)
+	throughputMean := throughput / float64(numThrMeasurements)
+
+	// now calculate variance
+	latencyVariance := 0.0
+	throughputVariance := 0.0
+	for _, m := range measurements {
+		latencyVariance += float64(m.numCommands-1) * (m.latencyVariance + m.latency - latencyMean)
+		throughputVariance += math.Pow(m.throughput-throughputMean, 2)
+	}
+	latencyVariance /= float64(numCommands - len(measurements))
+	throughputVariance /= float64(len(measurements) - 1)
 
 	return measurement{
-		latency:    sumLatencies / float64(len(measurements)),
-		throughput: sumThroughput,
-	}, nil
+		rate:               rate,
+		latency:            latencyMean,
+		latencyVariance:    latencyVariance,
+		throughput:         throughputMean,
+		throughputVariance: throughputVariance,
+		numCommands:        numCommands,
+		numThrMeasurements: numThrMeasurements,
+	}
 }
