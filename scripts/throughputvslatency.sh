2ee58ddb40fba12ebfd42a0cca63efd588257282
John Ingve Olsen <johningveolsen@gmail.com>
2020-05-14 18:07:14 +0200

    updated throughputvslatency.sh

--- scripts/throughputvslatency.sh
+++ scripts/throughputvslatency.sh
@@ -13,7 +13,7 @@ basedir="$1/throughputvslatency-$(date +%Y%m%d%H%M%S)"
 mkdir -p "$basedir"
 
 # how many times to repeat each benchmark
-for n in {1..1}; do
+for n in {1..2}; do
 
 	batch=100
 	payload=0
@@ -35,16 +35,16 @@ for n in {1..1}; do
 
 	# PAYLOAD
 
-	# batch=400
-	# payload=1024
-	# for t in {500,1000,2000,3000,5000,7000,10000,12000,15000,2000}; do
-	# 	run_benchmark "$basedir/b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 400 10 1
-	# done
+	batch=200
+	payload=1024
+	for t in {1000,2000,3000,4000,4250,4500,4750,5000,5250,5500,5750,6000,6250}; do
+		run_benchmark "$basedir/b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 400 60 1
+	done
 
-	# batch=400
-	# payload=128
-	# for t in {7000,10000,15000,20000,25000,27500,30000,35000,40000,45000}; do
-	# 	run_benchmark "$basedir/b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 1000 10 1
-	# done
+	batch=200
+	payload=128
+	for t in {7000,10000,15000,16000,17500,19000,20000,21000,22500,23000,23500}; do
+		run_benchmark "$basedir/b$batch-p$payload/$n/t$t" "$t" "$batch" "$payload" 1000 60 1
+	done
 
 done
