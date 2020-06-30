e1caf5939e707d33d108208635b3ef53ab98fdab
John Ingve Olsen <johningveolsen@gmail.com>
2020-06-30 09:42:50 +0200

    added option to kill one replica in run_servers.sh

--- scripts/run_servers.sh
+++ scripts/run_servers.sh
@@ -15,10 +15,15 @@ if [[ "$1" == "record" ]]; then
 		sudo sysctl kernel.perf_event_paranoid=1 || (echo "Failed to apply kernel parameter"; exit 1)
 	fi
 
-	run_with_rr 1 $@
-	run_with_rr 2 $@
-	run_with_rr 3 $@
-	run_with_rr 4 $@
+	run_with_rr 1 "${@:2}"
+	run_with_rr 2 "${@:2}"
+	run_with_rr 3 "${@:2}"
+	run_with_rr 4 "${@:2}"
+
+	if [ "$2" = "kill" ]; then
+		sleep 2s
+		kill $!
+	fi
 	
 	wait; wait; wait; wait;
 	exit 0
@@ -28,5 +33,10 @@ $bin --self-id 1 --privkey keys/r1.key --cpuprofile cpuprofile.out --memprofile 
 $bin --self-id 2 --privkey keys/r2.key "$@" > 2.out &
 $bin --self-id 3 --privkey keys/r3.key "$@" > 3.out &
 $bin --self-id 4 --privkey keys/r4.key "$@" > 4.out &
+
+if [ "$1" = "kill" ]; then
+	sleep 5s
+	kill $!
+fi
 
 wait; wait; wait; wait;
