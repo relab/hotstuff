460460b6628dda820ce3b5f1aa1ab288d1236c82
John Ingve Olsen <johningveolsen@gmail.com>
2021-01-11 13:23:29 +0100

    Reimplemented the ecdsa crypto using new interfaces

---
+++ crypto/ecdsa/ecdsa.go
@@ -0,0 +1,186 @@
+package ecdsa
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"sync"
+	"sync/atomic"
+
+	"github.com/relab/hotstuff"
+	"github.com/relab/hotstuff/internal/logging"
+)
+
+var logger = logging.GetLogger()
+
+var ErrHashMismatch = fmt.Errorf("Certificate hash does not match block hash")
+var ErrPartialDuplicate = fmt.Errorf("Cannot add more than one signature per replica")
+
+type PrivateKey struct {
+	*ecdsa.PrivateKey
+}
+
+func (pk PrivateKey) PublicKey() hotstuff.PublicKey {
+	return pk.Public()
+}
+
+var _ hotstuff.PrivateKey = (*PrivateKey)(nil)
+
+type ECDSASignature interface {
+	hotstuff.Signature
+
+	R() *big.Int
+	S() *big.Int
+	// RawBytes returns a raw byte string representation of the signature
+	RawBytes() string
+}
+
+type ecdsaSignature struct {
+	r, s *big.Int
+	id   hotstuff.ID
+}
+
+// Signer returns the ID of the replica that generated the signature.
+func (sig ecdsaSignature) Signer() hotstuff.ID {
+	return sig.id
+}
+
+func (sig *ecdsaSignature) R() *big.Int {
+	return sig.r
+}
+
+func (sig *ecdsaSignature) S() *big.Int {
+	return sig.s
+}
+
+// RawBytes returns a raw byte string representation of the signature
+func (sig *ecdsaSignature) RawBytes() string {
+	var b []byte
+	b = append(b, sig.r.Bytes()...)
+	b = append(b, sig.s.Bytes()...)
+	return string(b)
+}
+
+var _ ECDSASignature = (*ecdsaSignature)(nil)
+
+type ecdsaPartialCert struct {
+	sig  ECDSASignature
+	hash hotstuff.Hash
+}
+
+// Signature returns the signature
+func (cert ecdsaPartialCert) Signature() hotstuff.Signature {
+	return cert.sig
+}
+
+// BlockHash returns the hash of the block that was signed
+func (cert ecdsaPartialCert) BlockHash() *hotstuff.Hash {
+	panic("not implemented") // TODO: Implement
+}
+
+var _ hotstuff.PartialCert = (*ecdsaPartialCert)(nil)
+
+type ecdsaQuorumCert struct {
+	sigs map[hotstuff.ID]ECDSASignature
+	hash hotstuff.Hash
+}
+
+// BlockHash returns the hash of the block for which the certificate was created
+func (qc ecdsaQuorumCert) BlockHash() *hotstuff.Hash {
+	return &qc.hash
+}
+
+var _ hotstuff.QuorumCert = (*ecdsaQuorumCert)(nil)
+
+// TODO: consider adding caching back
+
+type ecdsaCrypto struct {
+	cfg hotstuff.Config
+}
+
+func New(cfg hotstuff.Config) (hotstuff.Signer, hotstuff.Verifier) {
+	ec := &ecdsaCrypto{cfg}
+	return ec, ec
+}
+
+func (ec *ecdsaCrypto) getPrivateKey() *PrivateKey {
+	pk := ec.cfg.PrivateKey()
+	return pk.(*PrivateKey)
+}
+
+// Sign signs a single block and returns the signature
+func (ec *ecdsaCrypto) Sign(block hotstuff.Block) (cert hotstuff.PartialCert, err error) {
+	hash := block.Hash()
+	r, s, err := ecdsa.Sign(rand.Reader, ec.getPrivateKey().PrivateKey, hash[:])
+	if err != nil {
+		return nil, err
+	}
+	return &ecdsaPartialCert{
+		&ecdsaSignature{r, s, ec.cfg.ID()},
+		*hash,
+	}, nil
+}
+
+// CreateQuourmCert creates a from a list of partial certificates
+func (ec *ecdsaCrypto) CreateQuorumCert(block hotstuff.Block, signatures []hotstuff.PartialCert) (cert hotstuff.QuorumCert, err error) {
+	hash := block.Hash()
+	qc := &ecdsaQuorumCert{
+		sigs: make(map[hotstuff.ID]ECDSASignature),
+		hash: *hash,
+	}
+	for _, s := range signatures {
+		if !bytes.Equal(hash[:], s.BlockHash()[:]) {
+			return nil, ErrHashMismatch
+		}
+		if _, ok := qc.sigs[s.Signature().Signer()]; ok {
+			return nil, ErrPartialDuplicate
+		}
+		qc.sigs[s.Signature().Signer()] = s.(ECDSASignature)
+	}
+	return qc, nil
+}
+
+// VerifyPartialCert verifies a single partial certificate
+func (ec *ecdsaCrypto) VerifyPartialCert(cert hotstuff.PartialCert) bool {
+	// TODO: decide how to handle incompatible types. For now we'll simply panic
+	sig := cert.Signature().(ECDSASignature)
+	replica, ok := ec.cfg.Replicas()[sig.Signer()]
+	if !ok {
+		logger.Info("ecdsaCrypto: got signature from replica whose ID (%d) was not in the config.")
+		return false
+	}
+	pk := replica.PublicKey().(*ecdsa.PublicKey)
+	return ecdsa.Verify(pk, (*cert.BlockHash())[:], sig.R(), sig.S())
+}
+
+// VerifyQuorumCert verifies a quorum certificate
+func (ec *ecdsaCrypto) VerifyQuorumCert(cert hotstuff.QuorumCert) bool {
+	qc := cert.(*ecdsaQuorumCert)
+	if len(qc.sigs) < ec.cfg.QuorumSize() {
+		return false
+	}
+
+	var wg sync.WaitGroup
+	var numVerified uint64 = 0
+	for id, psig := range qc.sigs {
+		info, ok := ec.cfg.Replicas()[id]
+		if !ok {
+			logger.Error("VerifyQuorumSig: got signature from replica whose ID (%d) was not in config.", id)
+		}
+		pubKey := info.PublicKey().(*ecdsa.PublicKey)
+		wg.Add(1)
+		go func(psig ECDSASignature) {
+			if ecdsa.Verify(pubKey, qc.BlockHash()[:], psig.R(), psig.S()) {
+				atomic.AddUint64(&numVerified, 1)
+			}
+			wg.Done()
+		}(psig)
+	}
+	wg.Wait()
+	return numVerified >= uint64(ec.cfg.QuorumSize())
+}
+
+var _ hotstuff.Signer = (*ecdsaCrypto)(nil)
+var _ hotstuff.Verifier = (*ecdsaCrypto)(nil)
