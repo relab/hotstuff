5d21b39a3b902e448b5d25e7252347a5bc13a3ba
John Ingve Olsen <johningveolsen@gmail.com>
2021-01-11 14:48:14 +0100

    Wrote an implementation of the BlockChain interface

---
+++ blockchain/blockchain.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import (
+	"container/list"
+	"sync"
+
+	"github.com/relab/hotstuff"
+)
+
+// blockChain stores a limited amount of blocks in a map.
+// blocks are evicted in LRU order.
+type blockChain struct {
+	mut         sync.Mutex
+	maxSize     int
+	blocks      map[hotstuff.Hash]*list.Element
+	accessOrder list.List
+}
+
+// New creates a new BlockChain with a maximum size
+func New(maxSize int) hotstuff.BlockChain {
+	return &blockChain{
+		maxSize: maxSize,
+		blocks:  make(map[hotstuff.Hash]*list.Element),
+	}
+}
+
+func (chain *blockChain) dropOldest() {
+	elem := chain.accessOrder.Back()
+	block := elem.Value.(hotstuff.Block)
+	delete(chain.blocks, *block.Hash())
+	chain.accessOrder.Remove(elem)
+}
+
+// Store stores a block in the blockchain
+func (chain *blockChain) Store(block hotstuff.Block) {
+	chain.mut.Lock()
+	defer chain.mut.Unlock()
+
+	if len(chain.blocks)+1 > chain.maxSize {
+		chain.dropOldest()
+	}
+
+	elem := chain.accessOrder.PushFront(block)
+	chain.blocks[*block.Hash()] = elem
+}
+
+// Get retrieves a block given its hash
+func (chain *blockChain) Get(hash *hotstuff.Hash) (hotstuff.Block, bool) {
+	chain.mut.Lock()
+	defer chain.mut.Unlock()
+
+	elem, ok := chain.blocks[*hash]
+	if !ok {
+		return nil, false
+	}
+
+	chain.accessOrder.MoveToFront(elem)
+
+	return elem.Value.(hotstuff.Block), true
+}
