43b93ed3f02c26777243e016acd5023e15a75a81
John Ingve Olsen <johningveolsen@gmail.com>
2021-02-01 15:14:38 +0100

    Add a diagram of the interfaces

--- hotstuff.go
+++ hotstuff.go
@@ -1,6 +1,44 @@
 // Package hotstuff defines the core types and interfaces that implement the HotStuff protocol.
 // These interfaces allow us to split the implementations into different modules,
 // and each module can have multiple can have multiple implementations.
+//
+// The following diagram illustrates the relationships between these interfaces:
+//                  OnDeliver()------------------------+
+//                                                     |
+//                  OnPropose()---------------------+  |
+//                                                  |  |
+//                  OnVote()---------------------+  |  |
+//                                               |  |  |
+//                  OnNewView()---------------+  |  |  |
+//                                            |  |  |  |
+//                                            v  v  v  v
+//  +--------------+                       +-------------------------+                     +----------------------+
+//  |              |                       |                         |<-----Propose()------|                      |
+//  |              |<--------Sign()--------|                         |                     |                      |
+//  |    Signer    |                       |                         |<-----NewView()------|                      |
+//  |              |<--CreateQuorumCert()--|                         |                     |                      |
+//  |              |                       |                         |------OnPropose()--->|   ViewSynchronizer   |
+//  +--------------+                       |                         |                     |                      |
+//                                         |        Consensus        |------OnNewView()--->|                      |
+//  +--------------+                       |                         |                     |                      |
+//  |              |                       |                         |------OnFinishQC()-->|                      |
+//  |              |<--VerifyQuorumCert()--|                         |                     +----------------------+
+//  | Verifier     |                       |                         |                                 ^
+//  |              |<-VerifyPartialCert()--|                         |                                 |
+//  |              |                       |                         |<---------GetLeader()------------+
+//  +--------------+                       +-|--|--|--|--------------+             |
+//                                           |  |  |  |                            |
+//  +-----------------+                      |  |  |  |                +-----------|----------+
+//  |                 |<----Propose()--------+  |  |  |                |                      |
+//  |                 |                         |  |  |                |                      |
+//  |                 |<----Vote()--------------+  |  |                |    LeaderRotation    |
+//  | Configuration / |                            |  |                |                      |
+//  | Replica         |<----NewView()--------------+  |                |                      |
+//  |                 |                               |                +----------------------+
+//  |                 |<----Fetch()-------------------+
+//  +-----------------+
+//
+//
 package hotstuff
 
 import (
