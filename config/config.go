dd620347ee852a9ee81eeb80b84e4f1b69e3eb1f
John Ingve Olsen <johningveolsen@gmail.com>
2021-02-12 15:30:15 +0100

    update doc for package config

--- config/config.go
+++ config/config.go
@@ -1,3 +1,7 @@
+// Package config contains structs that are useful for initializing hotstuff.
+//
+// These structs do not implement the hotstuff.Replica or hotstuff.Config interfaces,
+// but do contain more or less the same information.
 package config
 
 import (
@@ -7,14 +11,14 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-// ReplicaInfo holds information about a replica
+// ReplicaInfo holds information about a replica.
 type ReplicaInfo struct {
 	ID      hotstuff.ID
 	Address string
 	PubKey  *ecdsa.PublicKey
 }
 
-// ReplicaConfig holds information needed by a replica
+// ReplicaConfig holds information needed by a replica.
 type ReplicaConfig struct {
 	ID         hotstuff.ID
 	PrivateKey *ecdsa.PrivateKey
@@ -22,7 +26,7 @@ type ReplicaConfig struct {
 	Replicas   map[hotstuff.ID]*ReplicaInfo
 }
 
-// NewConfig returns a new ReplicaConfig instance
+// NewConfig returns a new ReplicaConfig instance.
 func NewConfig(id hotstuff.ID, privateKey *ecdsa.PrivateKey, creds credentials.TransportCredentials) *ReplicaConfig {
 	return &ReplicaConfig{
 		ID:         id,
