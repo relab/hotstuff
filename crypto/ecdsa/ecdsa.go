c40949b3272df410e034b75b976d99d74a986477
John Ingve Olsen <johningveolsen@gmail.com>
2021-02-13 15:03:17 +0100

    Update documentation for package crypto/ecdsa

--- crypto/ecdsa/ecdsa.go
+++ crypto/ecdsa/ecdsa.go
@@ -18,24 +18,33 @@ import (
 
 var logger = logging.GetLogger()
 
+// ErrHashMismatch is the error used when a partial certificate hash does not match the hash of a block.
 var ErrHashMismatch = fmt.Errorf("Certificate hash does not match block hash")
+
+// ErrPartialDuplicate is the error used when two or more signatures were created by the same replica.
 var ErrPartialDuplicate = fmt.Errorf("Cannot add more than one signature per replica")
 
+// PrivateKey is an ECDSA private key.
+//
+// This struct wraps the regular ecdsa.PrivateKey in order to implement the hotstuff.PrivateKey interface.
 type PrivateKey struct {
 	*ecdsa.PrivateKey
 }
 
+// PublicKey returns the public key associated with the private key
 func (pk PrivateKey) PublicKey() hotstuff.PublicKey {
 	return pk.Public()
 }
 
 var _ hotstuff.PrivateKey = (*PrivateKey)(nil)
 
+// Signature is an ECDSA signature
 type Signature struct {
 	r, s   *big.Int
 	signer hotstuff.ID
 }
 
+// NewSignature creates a new Signature struct from the given values.
 func NewSignature(r, s *big.Int, signer hotstuff.ID) *Signature {
 	return &Signature{r, s, signer}
 }
@@ -45,15 +54,17 @@ func (sig Signature) Signer() hotstuff.ID {
 	return sig.signer
 }
 
+// R returns the r value of the signature
 func (sig Signature) R() *big.Int {
 	return sig.r
 }
 
+// S returns the s value of the signature
 func (sig Signature) S() *big.Int {
 	return sig.s
 }
 
-// RawBytes returns a raw byte string representation of the signature
+// ToBytes returns a raw byte string representation of the signature
 func (sig Signature) ToBytes() []byte {
 	var b []byte
 	b = append(b, sig.r.Bytes()...)
@@ -63,25 +74,28 @@ func (sig Signature) ToBytes() []byte {
 
 var _ hotstuff.Signature = (*Signature)(nil)
 
+// PartialCert is an ECDSA signature and the hash that was signed.
 type PartialCert struct {
 	signature *Signature
 	hash      hotstuff.Hash
 }
 
+// NewPartialCert initializes a PartialCert struct from the given values.
 func NewPartialCert(signature *Signature, hash hotstuff.Hash) *PartialCert {
 	return &PartialCert{signature, hash}
 }
 
-// Signature returns the signature
+// Signature returns the signature.
 func (cert PartialCert) Signature() hotstuff.Signature {
 	return cert.signature
 }
 
-// BlockHash returns the hash of the block that was signed
+// BlockHash returns the hash of the block that was signed.
 func (cert PartialCert) BlockHash() hotstuff.Hash {
 	return cert.hash
 }
 
+// ToBytes returns a byte representation of the partial certificate.
 func (cert PartialCert) ToBytes() []byte {
 	return append(cert.hash[:], cert.signature.ToBytes()...)
 }
@@ -92,24 +106,28 @@ func (cert PartialCert) String() string {
 
 var _ hotstuff.PartialCert = (*PartialCert)(nil)
 
+// QuorumCert is a set of signature that form a quorum certificate for a block.
 type QuorumCert struct {
 	signatures map[hotstuff.ID]*Signature
 	hash       hotstuff.Hash
 }
 
+// NewQuorumCert initializes a new QuorumCert struct from the given values.
 func NewQuorumCert(signatures map[hotstuff.ID]*Signature, hash hotstuff.Hash) *QuorumCert {
 	return &QuorumCert{signatures, hash}
 }
 
+// Signatures returns the signatures within the quorum certificate.
 func (qc QuorumCert) Signatures() map[hotstuff.ID]*Signature {
 	return qc.signatures
 }
 
-// BlockHash returns the hash of the block for which the certificate was created
+// BlockHash returns the hash of the block for which the certificate was created.
 func (qc QuorumCert) BlockHash() hotstuff.Hash {
 	return qc.hash
 }
 
+// ToBytes returns a byte representation of the quorum certificate.
 func (qc QuorumCert) ToBytes() []byte {
 	b := qc.hash[:]
 	// sort signatures by id to ensure determinism
@@ -144,6 +162,7 @@ type ecdsaCrypto struct {
 	cfg hotstuff.Config
 }
 
+// New returns a new Signer and a new Verifier.
 func New(cfg hotstuff.Config) (hotstuff.Signer, hotstuff.Verifier) {
 	ec := &ecdsaCrypto{cfg}
 	return ec, ec
@@ -154,7 +173,7 @@ func (ec *ecdsaCrypto) getPrivateKey() *PrivateKey {
 	return pk.(*PrivateKey)
 }
 
-// Sign signs a single block and returns the signature
+// Sign signs a single block and returns a partial certificate.
 func (ec *ecdsaCrypto) Sign(block *hotstuff.Block) (cert hotstuff.PartialCert, err error) {
 	hash := block.Hash()
 	r, s, err := ecdsa.Sign(rand.Reader, ec.getPrivateKey().PrivateKey, hash[:])
@@ -167,7 +186,7 @@ func (ec *ecdsaCrypto) Sign(block *hotstuff.Block) (cert hotstuff.PartialCert, e
 	}, nil
 }
 
-// CreateQuourmCert creates a from a list of partial certificates
+// CreateQuorumCert creates a quorum certificate from a block and a set of signatures.
 func (ec *ecdsaCrypto) CreateQuorumCert(block *hotstuff.Block, signatures []hotstuff.PartialCert) (cert hotstuff.QuorumCert, err error) {
 	hash := block.Hash()
 	qc := &QuorumCert{
@@ -187,7 +206,7 @@ func (ec *ecdsaCrypto) CreateQuorumCert(block *hotstuff.Block, signatures []hots
 	return qc, nil
 }
 
-// VerifyPartialCert verifies a single partial certificate
+// VerifyPartialCert verifies a single partial certificate.
 func (ec *ecdsaCrypto) VerifyPartialCert(cert hotstuff.PartialCert) bool {
 	// TODO: decide how to handle incompatible types. For now we'll simply panic
 	sig := cert.Signature().(*Signature)
@@ -201,7 +220,7 @@ func (ec *ecdsaCrypto) VerifyPartialCert(cert hotstuff.PartialCert) bool {
 	return ecdsa.Verify(pk, hash[:], sig.R(), sig.S())
 }
 
-// VerifyQuorumCert verifies a quorum certificate
+// VerifyQuorumCert verifies a quorum certificate.
 func (ec *ecdsaCrypto) VerifyQuorumCert(cert hotstuff.QuorumCert) bool {
 	// If QC was created for genesis, then skip verification.
 	if cert.BlockHash() == hotstuff.GetGenesis().Hash() {
