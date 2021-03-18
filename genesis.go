85718ab11d73421ee04c1589222bc06d565faaf4
John Ingve Olsen <johningveolsen@gmail.com>
2021-03-18 11:07:02 +0100

    create genesis block using NewBlock
    We now calculate the hash in NewBlock.

--- genesis.go
+++ genesis.go
@@ -1,12 +1,8 @@
 package hotstuff
 
-var genesisBlock = Block{
-	cert:     nil,
-	view:     0,
-	proposer: 0,
-}
+var genesisBlock = NewBlock(Hash{}, nil, "", 0, 0)
 
 // GetGenesis returns a pointer to the genesis block, the starting point for the hotstuff blockchain.
 func GetGenesis() *Block {
-	return &genesisBlock
+	return genesisBlock
 }
