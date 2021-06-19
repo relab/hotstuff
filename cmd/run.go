d97c1d27298f9046dfffe9b2db8f667cbcfdaad6
John Ingve Olsen <johningveolsen@gmail.com>
2021-06-19 12:55:50 +0200

    deploy to remote hosts

--- cmd/run.go
+++ cmd/run.go
@@ -2,18 +2,18 @@ package cmd
 
 import (
 	"log"
+	"os"
 	"strconv"
 	"time"
 
+	"github.com/Raytar/iago"
 	"github.com/relab/hotstuff/internal/orchestration"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var (
-	// flags
-	worker         bool
-	remotePort     int
+	// experiment options
 	numReplicas    int
 	numClients     int
 	batchSize      int
@@ -24,7 +24,13 @@ var (
 	consensusName  string
 	cryptoName     string
 	leaderRotation string
-	hosts          []string
+
+	// worker flags
+	remotePort    int
+	worker        bool
+	hosts         []string
+	exePath       string
+	sshConfigPath string
 
 	// runCmd represents the run command
 	runCmd = &cobra.Command{
@@ -40,8 +46,6 @@ var (
 func init() {
 	rootCmd.AddCommand(runCmd)
 
-	runCmd.Flags().BoolVar(&worker, "worker", false, "run a local worker")
-	runCmd.Flags().IntVar(&remotePort, "port", 4000, "the port to start remote workers on")
 	runCmd.Flags().IntVar(&numReplicas, "replicas", 4, "number of replicas to run")
 	runCmd.Flags().IntVar(&numClients, "clients", 1, "number of clients to run")
 	runCmd.Flags().IntVar(&batchSize, "batch-size", 1, "number of commands to batch together in each block")
@@ -52,7 +56,13 @@ func init() {
 	runCmd.Flags().StringVar(&consensusName, "consensus", "chainedhotstuff", "name of the consensus implementation")
 	runCmd.Flags().StringVar(&cryptoName, "crypto", "ecdsa", "name of the crypto implementation")
 	runCmd.Flags().StringVar(&leaderRotation, "leader-rotation", "round-robin", "name of the leader rotation algorithm")
+
+	runCmd.Flags().IntVar(&remotePort, "port", 4000, "the port to start remote workers on")
+	runCmd.Flags().BoolVar(&worker, "worker", false, "run a local worker")
 	runCmd.Flags().StringSliceVar(&hosts, "hosts", nil, "the remote hosts to run the experiment on via ssh")
+	runCmd.Flags().StringVar(&exePath, "exe", "", "path to the executable to deploy and run on remote workers")
+	runCmd.Flags().StringVar(&sshConfigPath, "ssh-config", "", "path to ssh_config file to resolve host aliases (defaults to ~/.ssh/config)")
+
 	viper.BindPFlags(runCmd.Flags())
 }
 
@@ -70,13 +80,30 @@ func runController() {
 		LeaderRotation: leaderRotation,
 	}
 
+	g, err := iago.NewSSHGroup(hosts, sshConfigPath)
+	if err != nil {
+		log.Fatalln("Failed to connect to remote hosts: ", err)
+	}
+
+	if exePath == "" {
+		exePath, err = os.Executable()
+		if err != nil {
+			log.Fatalln("Failed to get executable path: ", err)
+		}
+	}
+
+	err = orchestration.Deploy(g, exePath, strconv.Itoa(remotePort))
+	if err != nil {
+		log.Fatalln("Failed to deploy workers: ", err)
+	}
+
 	if worker {
 		go runWorker(remotePort)
 		hosts = append(hosts, "localhost:"+strconv.Itoa(remotePort))
 	}
 
 	hosts := viper.GetStringSlice("hosts")
-	err := experiment.Run(hosts)
+	err = experiment.Run(hosts)
 	if err != nil {
 		log.Fatal(err)
 	}
