550bf0fc0f56b514487bb4d83791fad409a279d5
John Ingve Olsen <johningveolsen@gmail.com>
2021-07-08 11:44:13 +0200

    deploy_test: set a higher timeout value

--- scripts/deploy_test.sh
+++ scripts/deploy_test.sh
@@ -9,6 +9,6 @@ compose_args="--project-name=hotstuff"
 docker-compose $compose_args up -d --build
 
 docker-compose $compose_args exec controller /bin/sh -c "ssh-keyscan -H hotstuff_worker_1 >> ~/.ssh/known_hosts"
-docker-compose $compose_args exec controller /bin/sh -c "hotstuff run --hosts 'hotstuff_worker_1'" -e "HOTSTUFF_LOG=info"
+docker-compose $compose_args exec controller /bin/sh -c "hotstuff run --hosts 'hotstuff_worker_1' --connect-timeout 5s"
 
 docker-compose $compose_args down
