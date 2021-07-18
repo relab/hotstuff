7b3172e9aa64cb791d02bc66679f1c577a7dd783
John Ingve Olsen <johningveolsen@gmail.com>
2021-07-18 17:40:54 +0200

    orchestration_test: test bls12 and fasthotstuff

--- internal/orchestration/orchestration_test.go
+++ internal/orchestration/orchestration_test.go
@@ -11,40 +11,47 @@ import (
 )
 
 func TestOrchestration(t *testing.T) {
-	controllerStream, workerStream := net.Pipe()
+	run := func(consensusImpl string, crypto string) {
+		controllerStream, workerStream := net.Pipe()
 
-	workerProxy := orchestration.NewRemoteWorker(protostream.NewWriter(controllerStream), protostream.NewReader(controllerStream))
-	worker := orchestration.NewWorker(protostream.NewWriter(workerStream), protostream.NewReader(workerStream), consensus.NopLogger())
+		workerProxy := orchestration.NewRemoteWorker(protostream.NewWriter(controllerStream), protostream.NewReader(controllerStream))
+		worker := orchestration.NewWorker(protostream.NewWriter(workerStream), protostream.NewReader(workerStream), consensus.NopLogger())
 
-	experiment := &orchestration.Experiment{
-		NumReplicas:       4,
-		NumClients:        2,
-		BatchSize:         100,
-		MaxConcurrent:     250,
-		PayloadSize:       100,
-		ConnectTimeout:    1 * time.Second,
-		ViewTimeout:       1000,
-		TimoutSamples:     1000,
-		TimeoutMultiplier: 1.2,
-		Duration:          1 * time.Second,
-		Consensus:         "chainedhotstuff",
-		Crypto:            "ecdsa",
-		LeaderRotation:    "round-robin",
-		Hosts:             map[string]orchestration.RemoteWorker{"127.0.0.1": workerProxy},
+		experiment := &orchestration.Experiment{
+			NumReplicas:       4,
+			NumClients:        2,
+			BatchSize:         100,
+			MaxConcurrent:     250,
+			PayloadSize:       100,
+			ConnectTimeout:    1 * time.Second,
+			ViewTimeout:       1000,
+			TimoutSamples:     1000,
+			TimeoutMultiplier: 1.2,
+			Duration:          1 * time.Second,
+			Consensus:         consensusImpl,
+			Crypto:            crypto,
+			LeaderRotation:    "round-robin",
+			Hosts:             map[string]orchestration.RemoteWorker{"127.0.0.1": workerProxy},
+		}
+
+		c := make(chan error)
+		go func() {
+			c <- worker.Run()
+		}()
+
+		err := experiment.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = <-c
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	c := make(chan error)
-	go func() {
-		c <- worker.Run()
-	}()
-
-	err := experiment.Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = <-c
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("ChainedHotStuff+ECDSA", func(t *testing.T) { run("chainedhotstuff", "ecdsa") })
+	t.Run("ChainedHotStuff+BLS12", func(t *testing.T) { run("chainedhotstuff", "bls12") })
+	t.Run("Fast-HotStuff+ECDSA", func(t *testing.T) { run("fasthotstuff", "ecdsa") })
+	t.Run("Fast-HotStuff+BLS12", func(t *testing.T) { run("fasthotstuff", "bls12") })
 }
