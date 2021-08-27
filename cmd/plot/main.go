9cd4b1469fc637fd4acb025c831588a2adb9baac
John Ingve Olsen <johningveolsen@gmail.com>
2021-08-27 09:56:49 +0200

    update plot command

--- cmd/plot/main.go
+++ cmd/plot/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -10,25 +12,51 @@ import (
 	_ "github.com/relab/hotstuff/metrics/types"
 )
 
+var (
+	interval            = flag.Duration("interval", time.Second, "Length of time interval to group measurements by.")
+	latency             = flag.String("latency", "", "File to save latency plot to.")
+	throughput          = flag.String("throughput", "", "File to save throughput plot to.")
+	throughputVSLatency = flag.String("throughputvslatency", "", "File to save throughput vs latency plot to.")
+)
+
 func main() {
-	file, err := os.Open(os.Args[1])
+	flag.Parse()
+
+	srcPath := flag.Arg(0)
+	if srcPath == "" {
+		fmt.Fprintf(os.Stderr, "usage: %s [flags] [path to measurements]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	file, err := os.Open(srcPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	latencyPlot := plotting.NewClientLatencyPlot()
 	throughputPlot := plotting.NewThroughputPlot()
 	throughputVSLatencyPlot := plotting.NewThroughputVSLatencyPlot()
+
 	reader := plotting.NewReader(file, &latencyPlot, &throughputPlot, &throughputVSLatencyPlot)
 	if err := reader.ReadAll(); err != nil {
 		log.Fatalln(err)
 	}
-	if err := latencyPlot.PlotAverage(os.Args[2], time.Second); err != nil {
-		log.Fatalln(err)
+
+	if *latency != "" {
+		if err := latencyPlot.PlotAverage(*latency, *interval); err != nil {
+			log.Fatalln(err)
+		}
 	}
-	if err := throughputPlot.PlotAverage(os.Args[3], time.Second); err != nil {
-		log.Fatalln(err)
+
+	if *throughput != "" {
+		if err := throughputPlot.PlotAverage(*throughput, *interval); err != nil {
+			log.Fatalln(err)
+		}
 	}
-	if err := throughputVSLatencyPlot.PlotAverage(os.Args[4], time.Second); err != nil {
-		log.Fatalln(err)
+
+	if *throughputVSLatency != "" {
+		if err := throughputVSLatencyPlot.PlotAverage(*throughputVSLatency, *interval); err != nil {
+			log.Fatalln(err)
+		}
 	}
 }
