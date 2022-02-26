cda20be61e42c9e4dcd233d6310cc8b27ed04152
John Ingve Olsen <johningveolsen@gmail.com>
2022-02-26 17:17:48 +0100

    Update Crypto implementation

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -6,27 +6,27 @@ import (
 	"github.com/relab/hotstuff/consensus"
 )
 
-type base struct {
-	consensus.CryptoImpl
+type crypto struct {
+	consensus.CryptoBase
 }
 
-// New returns a new base implementation of the Crypto interface. It will use the given CryptoImpl to create and verify
+// New returns a new implementation of the Crypto interface. It will use the given CryptoBase to create and verify
 // signatures.
-func New(impl consensus.CryptoImpl) consensus.Crypto {
-	return base{CryptoImpl: impl}
+func New(impl consensus.CryptoBase) consensus.Crypto {
+	return crypto{CryptoBase: impl}
 }
 
 // InitConsensusModule gives the module a reference to the Modules object.
 // It also allows the module to set module options using the OptionsBuilder.
-func (base base) InitConsensusModule(mods *consensus.Modules, cfg *consensus.OptionsBuilder) {
-	if mod, ok := base.CryptoImpl.(consensus.Module); ok {
+func (c crypto) InitConsensusModule(mods *consensus.Modules, cfg *consensus.OptionsBuilder) {
+	if mod, ok := c.CryptoBase.(consensus.Module); ok {
 		mod.InitConsensusModule(mods, cfg)
 	}
 }
 
 // CreatePartialCert signs a single block and returns the partial certificate.
-func (base base) CreatePartialCert(block *consensus.Block) (cert consensus.PartialCert, err error) {
-	sig, err := base.Sign(block.Hash())
+func (c crypto) CreatePartialCert(block *consensus.Block) (cert consensus.PartialCert, err error) {
+	sig, err := c.Sign(block.Hash())
 	if err != nil {
 		return consensus.PartialCert{}, err
 	}
@@ -34,16 +34,16 @@ func (base base) CreatePartialCert(block *consensus.Block) (cert consensus.Parti
 }
 
 // CreateQuorumCert creates a quorum certificate from a list of partial certificates.
-func (base base) CreateQuorumCert(block *consensus.Block, signatures []consensus.PartialCert) (cert consensus.QuorumCert, err error) {
+func (c crypto) CreateQuorumCert(block *consensus.Block, signatures []consensus.PartialCert) (cert consensus.QuorumCert, err error) {
 	// genesis QC is always valid.
 	if block.Hash() == consensus.GetGenesis().Hash() {
 		return consensus.NewQuorumCert(nil, 0, consensus.GetGenesis().Hash()), nil
 	}
-	sigs := make([]consensus.Signature, 0, len(signatures))
+	sigs := make([]consensus.QuorumSignature, 0, len(signatures))
 	for _, sig := range signatures {
 		sigs = append(sigs, sig.Signature())
 	}
-	sig, err := base.CreateThresholdSignature(sigs, block.Hash())
+	sig, err := c.Combine(sigs...)
 	if err != nil {
 		return consensus.QuorumCert{}, err
 	}
@@ -51,36 +51,34 @@ func (base base) CreateQuorumCert(block *consensus.Block, signatures []consensus
 }
 
 // CreateTimeoutCert creates a timeout certificate from a list of timeout messages.
-func (base base) CreateTimeoutCert(view consensus.View, timeouts []consensus.TimeoutMsg) (cert consensus.TimeoutCert, err error) {
+func (c crypto) CreateTimeoutCert(view consensus.View, timeouts []consensus.TimeoutMsg) (cert consensus.TimeoutCert, err error) {
 	// view 0 is always valid.
 	if view == 0 {
 		return consensus.NewTimeoutCert(nil, 0), nil
 	}
-	sigs := make([]consensus.Signature, 0, len(timeouts))
+	sigs := make([]consensus.QuorumSignature, 0, len(timeouts))
 	for _, timeout := range timeouts {
 		sigs = append(sigs, timeout.ViewSignature)
 	}
-	sig, err := base.CreateThresholdSignature(sigs, view.ToHash())
+	sig, err := c.Combine(sigs...)
 	if err != nil {
 		return consensus.TimeoutCert{}, err
 	}
 	return consensus.NewTimeoutCert(sig, view), nil
 }
 
-func (base base) CreateAggregateQC(view consensus.View, timeouts []consensus.TimeoutMsg) (aggQC consensus.AggregateQC, err error) {
+func (c crypto) CreateAggregateQC(view consensus.View, timeouts []consensus.TimeoutMsg) (aggQC consensus.AggregateQC, err error) {
 	qcs := make(map[hotstuff.ID]consensus.QuorumCert)
-	sigs := make([]consensus.Signature, 0, len(timeouts))
-	hashes := make(map[hotstuff.ID]consensus.Hash)
+	sigs := make([]consensus.QuorumSignature, 0, len(timeouts))
 	for _, timeout := range timeouts {
 		if qc, ok := timeout.SyncInfo.QC(); ok {
 			qcs[timeout.ID] = qc
 		}
 		if timeout.MsgSignature != nil {
 			sigs = append(sigs, timeout.MsgSignature)
-			hashes[timeout.ID] = timeout.Hash()
 		}
 	}
-	sig, err := base.CreateThresholdSignatureForMessageSet(sigs, hashes)
+	sig, err := c.Combine(sigs...)
 	if err != nil {
 		return aggQC, err
 	}
@@ -88,28 +86,28 @@ func (base base) CreateAggregateQC(view consensus.View, timeouts []consensus.Tim
 }
 
 // VerifyPartialCert verifies a single partial certificate.
-func (base base) VerifyPartialCert(cert consensus.PartialCert) bool {
-	return base.Verify(cert.Signature(), cert.BlockHash())
+func (c crypto) VerifyPartialCert(cert consensus.PartialCert) bool {
+	return c.Verify(cert.Signature(), consensus.VerifyHash(cert.BlockHash()))
 }
 
 // VerifyQuorumCert verifies a quorum certificate.
-func (base base) VerifyQuorumCert(qc consensus.QuorumCert) bool {
+func (c crypto) VerifyQuorumCert(qc consensus.QuorumCert) bool {
 	if qc.BlockHash() == consensus.GetGenesis().Hash() {
 		return true
 	}
-	return base.VerifyThresholdSignature(qc.Signature(), qc.BlockHash())
+	return c.Verify(qc.Signature(), consensus.VerifyHash(qc.BlockHash()))
 }
 
 // VerifyTimeoutCert verifies a timeout certificate.
-func (base base) VerifyTimeoutCert(tc consensus.TimeoutCert) bool {
+func (c crypto) VerifyTimeoutCert(tc consensus.TimeoutCert) bool {
 	if tc.View() == 0 {
 		return true
 	}
-	return base.VerifyThresholdSignature(tc.Signature(), tc.View().ToHash())
+	return c.Verify(tc.Signature(), consensus.VerifyHash(tc.View().ToHash()))
 }
 
 // VerifyAggregateQC verifies the AggregateQC and returns the highQC, if valid.
-func (base base) VerifyAggregateQC(aggQC consensus.AggregateQC) (bool, consensus.QuorumCert) {
+func (c crypto) VerifyAggregateQC(aggQC consensus.AggregateQC) (bool, consensus.QuorumCert) {
 	var highQC *consensus.QuorumCert
 	hashes := make(map[hotstuff.ID]consensus.Hash)
 	for id, qc := range aggQC.QCs() {
@@ -127,11 +125,11 @@ func (base base) VerifyAggregateQC(aggQC consensus.AggregateQC) (bool, consensus
 			SyncInfo: consensus.NewSyncInfo().WithQC(qc),
 		}.Hash()
 	}
-	ok := base.VerifyThresholdSignatureForMessageSet(aggQC.Sig(), hashes)
+	ok := c.Verify(aggQC.Sig(), consensus.VerifyHashes(hashes))
 	if !ok {
 		return false, consensus.QuorumCert{}
 	}
-	if base.VerifyQuorumCert(*highQC) {
+	if c.VerifyQuorumCert(*highQC) {
 		return true, *highQC
 	}
 	return false, consensus.QuorumCert{}
