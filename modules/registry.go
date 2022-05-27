939909f3c92f0c1a88de6f7db486ac0f8cc0eb8a
John Ingve Olsen <johningveolsen@gmail.com>
2022-05-27 13:08:20 +0200

    modules/registry: use generics to constrain types

--- modules/registry.go
+++ modules/registry.go
@@ -12,18 +12,11 @@ var (
 	byName      = make(map[string]interface{})
 )
 
-// RegisterModule registers a module implementation with the specified name.
-// constructor must be a function returning the interface of the module.
+// RegisterModule registers a constructor for a module implementation with the specified name.
 // For example:
 //  RegisterModule("chainedhotstuff", func() consensus.Rules { return chainedhotstuff.New() })
-func RegisterModule(name string, constructor interface{}) {
-	ctorType := reflect.TypeOf(constructor)
-
-	if ctorType.Kind() != reflect.Func && ctorType.NumOut() != 1 && ctorType.Out(0).Kind() != reflect.Interface {
-		panic("invalid argument: constructor must be a function returning an interface")
-	}
-
-	ifaceType := ctorType.Out(0)
+func RegisterModule[T any](name string, constructor func() T) {
+	moduleType := reflect.TypeOf(constructor).Out(0)
 
 	registryMut.Lock()
 	defer registryMut.Unlock()
@@ -33,29 +26,22 @@ func RegisterModule(name string, constructor interface{}) {
 	}
 	byName[name] = constructor
 
-	moduleRegistry, ok := byInterface[ifaceType]
+	moduleRegistry, ok := byInterface[moduleType]
 	if !ok {
 		moduleRegistry = make(map[string]interface{})
-		byInterface[ifaceType] = moduleRegistry
+		byInterface[moduleType] = moduleRegistry
 	}
 
 	moduleRegistry[name] = constructor
 }
 
-// GetModule retrieves a new instance of the module with the specified name.
-// out must be a non-nil pointer to a variable with the interface type of the module.
+// GetModule constructs a new instance of the module with the specified name.
 // GetModule returns true if the module is found, false otherwise.
 // For example:
 //  var rules consensus.Rules
 //  GetModule("chainedhotstuff", &rules)
-func GetModule(name string, out interface{}) bool {
-	outType := reflect.TypeOf(out)
-
-	if outType.Kind() != reflect.Ptr {
-		panic("invalid argument: out must be a non-nil pointer to an interface variable")
-	}
-
-	targetType := outType.Elem()
+func GetModule[T any](name string, out *T) bool {
+	targetType := reflect.TypeOf(out).Elem()
 
 	registryMut.Lock()
 	defer registryMut.Unlock()
@@ -75,7 +61,7 @@ func GetModule(name string, out interface{}) bool {
 }
 
 // GetModuleUntyped returns a new instance of the named module, if it exists.
-func GetModuleUntyped(name string) (v interface{}, ok bool) {
+func GetModuleUntyped(name string) (m any, ok bool) {
 	registryMut.Lock()
 	defer registryMut.Unlock()
 
@@ -84,9 +70,9 @@ func GetModuleUntyped(name string) (v interface{}, ok bool) {
 		return nil, false
 	}
 
-	reflect.ValueOf(&v).Elem().Set(reflect.ValueOf(ctor).Call([]reflect.Value{})[0])
+	reflect.ValueOf(&m).Elem().Set(reflect.ValueOf(ctor).Call([]reflect.Value{})[0])
 
-	return v, ok
+	return m, ok
 }
 
 // ListModules returns a map of interface names to module names.
