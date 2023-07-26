48bbb674320f502d0d7ea07d95f5a6d72889cb33
John Ingve Olsen <johningveolsen@gmail.com>
2023-07-26 16:29:29 +0200

    modules/registry: add a test for ListModules

--- modules/registry_test.go
+++ modules/registry_test.go
@@ -1,16 +1,34 @@
 package modules_test
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/relab/hotstuff/modules"
+	"golang.org/x/exp/slices"
 )
 
-func TestModuleRegistry(t *testing.T) {
+func init() {
+	// register some modules
 	modules.RegisterModule("frobulator", func() moduleIface {
 		return module{}
 	})
 
+	modules.RegisterModule("fizzbuzz", func() moduleIface {
+		return fizzbuzz{}
+	})
+
+	modules.RegisterModule("fizzer", func() fizzer {
+		return fizzbuzz{}
+	})
+
+	modules.RegisterModule("buzzer", func() buzzer {
+		return fizzbuzz{}
+	})
+}
+
+func TestModuleRegistry(t *testing.T) {
 	frobulator, ok := modules.GetModule[moduleIface]("frobulator")
 	if !ok {
 		t.Fatal("module was not found")
@@ -23,6 +41,32 @@ func TestModuleRegistry(t *testing.T) {
 	}
 }
 
+func TestListModules(t *testing.T) {
+	getName := func(t reflect.Type) string {
+		return fmt.Sprintf("(%s).%s", t.PkgPath(), t.Name())
+	}
+
+	want := map[string][]string{
+		getName(reflect.TypeOf((*moduleIface)(nil)).Elem()): {"frobulator", "fizzbuzz"},
+		getName(reflect.TypeOf((*fizzer)(nil)).Elem()):      {"fizzer"},
+		getName(reflect.TypeOf((*buzzer)(nil)).Elem()):      {"buzzer"},
+	}
+
+	list := modules.ListModules()
+
+	for iface, names := range want {
+		if got, ok := list[iface]; !ok {
+			t.Errorf("expected interface '%s' to be in list of modules", iface)
+		} else {
+			for _, name := range names {
+				if !slices.Contains(got, name) {
+					t.Errorf("expected '%s' to be in list of '%s' implementations", name, iface)
+				}
+			}
+		}
+	}
+}
+
 type moduleIface interface {
 	frobulate(i *int)
 }
@@ -32,3 +76,19 @@ type module struct{}
 func (module) frobulate(i *int) {
 	*i++
 }
+
+type fizzer interface {
+	fizz(i int)
+}
+
+type buzzer interface {
+	buzz(i int)
+}
+
+type fizzbuzz struct{}
+
+func (f fizzbuzz) fizz(_ int) { panic("not implemented") }
+
+func (f fizzbuzz) buzz(_ int) { panic("not implemented") }
+
+func (f fizzbuzz) frobulate(_ *int) { panic("not implemented") }
