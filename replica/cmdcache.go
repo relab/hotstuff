e906680f0eddec67d896a7a3531a1933753c919d
AlanRostem <arostem@gmail.com>
2024-10-23 17:09:54 +0200

    Fixed a crash during debugging commands.

--- replica/cmdcache.go
+++ replica/cmdcache.go
@@ -48,8 +48,12 @@ func (c *cmdCache) addCommand(cmd *clientpb.Command) {
 	c.mut.Lock()
 	defer c.mut.Unlock()
 	if serialNo := c.serialNumbers[cmd.GetClientID()]; serialNo >= cmd.GetSequenceNumber() {
+		var s []byte
+		if len(cmd.Data) >= 4 {
+			s = cmd.Data[len(cmd.Data)-4:]
+		}
 		// command is too old
-		c.logger.Debugf("addCommand: command too old: %x", cmd.Data[len(cmd.Data)-4:])
+		c.logger.Debugf("addCommand: command too old: %x", s)
 		return
 	}
 	c.cache.PushBack(cmd)
