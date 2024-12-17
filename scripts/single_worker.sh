a7b75b1b8f06b14f2533091bbb7274ee14650675
Hein Meling <hein.meling@uis.no>
2024-12-17 21:44:44 +0100

    fix: deploy_test.sh now uses "docker compose" and removed version
    This formats the scripts and removes the version line from the
    docker-compose.yml file. The deploy_test.sh script also replaces
    docker-compose with "docker compose" since GitHub now only supports
    version 2 and higher of docker compose.

--- scripts/single_worker.sh
+++ scripts/single_worker.sh
@@ -7,14 +7,14 @@ if [ ! -f "./id" ]; then
 fi
 
 # ensure that the image is built
-docker images | grep "$image" &>/dev/null \
-	|| docker build -t "$image" -f "./Dockerfile.worker" ".."
+docker images | grep "$image" &>/dev/null ||
+	docker build -t "$image" -f "./Dockerfile.worker" ".."
 
 container="$(docker run --rm -d -p 2020:22 -p 4000:4000 "$image")"
 
 sleep 1s
 
-ssh-keyscan -p 2020 127.0.0.1 localhost > known_hosts
+ssh-keyscan -p 2020 127.0.0.1 localhost >known_hosts
 
 docker logs --follow "$container"
 docker rm -f "$container" &>/dev/null
