c5cda6830c82ed9375ae2bc213500c4521647118
AlanRostem <arostem@gmail.com>
2025-01-07 10:10:32 +0100

    Switched to go1.24 tool directive.

--- tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-
-package hotstuff
-
-import (
-	_ "github.com/golang/mock/mockgen"
-	_ "github.com/relab/gorums/cmd/protoc-gen-gorums"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-)
