99821355db6fdf481541f8f780d32f4493938c16
AlanRostem <arostem@gmail.com>
2025-01-14 15:08:06 +0100

    Fix docker tests

--- scripts/deploy_test.sh
+++ scripts/deploy_test.sh
@@ -23,7 +23,7 @@ compose_args="--project-name=hotstuff"
 docker compose $compose_args up -d --build --scale worker=4
 
 docker compose $compose_args exec -T controller /bin/sh -c "ssh-keyscan -H $(join ' ' "${hosts[@]}") >> ~/.ssh/known_hosts" &>/dev/null
-docker compose $compose_args exec -T controller /bin/sh -c "hotstuff run --hosts '$(join ',' "${hosts[@]}")' --config=./example_config.cue --log-level info"
+docker compose $compose_args exec -T controller /bin/sh -c "hotstuff run --config=./example_config.cue"
 exit_code="$?"
 
 docker compose $compose_args down
