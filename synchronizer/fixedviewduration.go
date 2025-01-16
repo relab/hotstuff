9cc065bfd3bd86851a40182009a6c7dbb565b006
Hein Meling <hein.meling@uis.no>
2025-01-16 09:18:37 +0100

    doc: added doc comments for fixedviewduration.go

--- synchronizer/fixedviewduration.go
+++ synchronizer/fixedviewduration.go
@@ -6,19 +6,23 @@ type fixedViewDuration struct {
 	duration time.Duration
 }
 
+// NewFixedViewDuration returns a ViewDuration with a fixed duration.
 func NewFixedViewDuration(duration time.Duration) ViewDuration {
 	return &fixedViewDuration{
 		duration: duration,
 	}
 }
 
+// Duration returns the fixed duration.
 func (f *fixedViewDuration) Duration() time.Duration {
 	return f.duration
 }
 
-// fixed view duration does not need to keep track of anything
+// ViewStarted does nothing for the FixedViewDuration.
 func (f *fixedViewDuration) ViewStarted() {}
 
+// ViewSucceeded does nothing for the FixedViewDuration.
 func (f *fixedViewDuration) ViewSucceeded() {}
 
+// ViewTimeout does nothing for the FixedViewDuration.
 func (f *fixedViewDuration) ViewTimeout() {}
