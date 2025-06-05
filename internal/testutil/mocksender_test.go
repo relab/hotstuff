dc2191c263306ef3ff8c4da82542f6c4d42b1414
AlanRostem <arostem@gmail.com>
2025-06-05 10:28:58 +0200

    Added a test for MockSender.RequestBlock.

--- internal/testutil/mocksender_test.go
+++ internal/testutil/mocksender_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/relab/hotstuff/security/blockchain"
 	"github.com/relab/hotstuff/security/cert"
 	"github.com/relab/hotstuff/security/crypto/ecdsa"
+	"github.com/relab/hotstuff/wiring"
 )
 
 type replica struct {
@@ -126,5 +127,45 @@ func TestSub(t *testing.T) {
 	_, err = sender.Sub([]hotstuff.ID{5, 6})
 	if err == nil {
 		t.Fatal("expected an error")
+	}
+}
+
+func TestRequestBlock(t *testing.T) {
+	replicaIDs := []hotstuff.ID{1, 2, 3, 4}
+	chains := make([]*blockchain.BlockChain, 0)
+	senders := make([]*testutil.MockSender, 0)
+	var firstCore *wiring.Core
+	for _, id := range replicaIDs {
+		depsCore := wiring.NewCore(id, "test", testutil.GenerateECDSAKey(t))
+		if firstCore == nil {
+			firstCore = depsCore
+		}
+		sender := testutil.NewMockSender(id, replicaIDs...)
+		chain := blockchain.New(
+			depsCore.EventLoop(),
+			depsCore.Logger(),
+			sender,
+		)
+		senders = append(senders, sender)
+		chains = append(chains, chain)
+	}
+	for _, chain := range chains {
+		for _, sender := range senders {
+			sender.AddBlockChain(chain)
+		}
+	}
+	signer := cert.NewAuthority(
+		firstCore.RuntimeCfg(),
+		chains[0],
+		ecdsa.New(firstCore.RuntimeCfg()),
+	)
+	sender0 := senders[0]
+	chain1 := chains[1]
+	block := testutil.CreateBlock(t, signer)
+	chain1.Store(block)
+
+	_, ok := sender0.RequestBlock(nil, block.Hash())
+	if !ok {
+		t.Fatal("expected a block to be returned")
 	}
 }
