a30b787af8421f192eb5f3d438d3080b34b219b7
Hein Meling <hein.meling@uis.no>
2025-06-21 00:31:38 +0200

    doc(consensus): improve interface docs for VoteRuler, CommitRuler, ProposeRuler, and Ruleset
    This tries to follow Go documentation conventions.

--- protocol/consensus/ruleset.go
+++ protocol/consensus/ruleset.go
@@ -5,30 +5,35 @@ import (
 	"github.com/relab/hotstuff/internal/proto/clientpb"
 )
 
-// VoteRuler is an interface that specifies when to vote for a block for a given view.
+// VoteRuler is the interface that wraps the VoteRule method.
+//
+// The VoteRule method decides whether or not to vote for a block in a given view.
 type VoteRuler interface {
-	// VoteRule decides whether to vote for the block.
+	// VoteRule returns true if the proposal's block should be voted for in the given view.
 	VoteRule(hotstuff.View, hotstuff.ProposeMsg) bool
 }
 
-// CommitRuler is an interface that specifies when to commit a block and its ancestors
-// on the local chain.
+// CommitRuler is the interface that wraps the CommitRule method.
+//
+// The CommitRule method is used to determine the youngest ancestor of a given block
+// that can be committed on the local chain.
 type CommitRuler interface {
-	// CommitRule decides whether any ancestor of the block can be committed.
-	// Returns the youngest ancestor of the block that can be committed.
+	// CommitRule returns the youngest ancestor of the given block that can be committed.
 	CommitRule(*hotstuff.Block) *hotstuff.Block
 }
 
-// ProposeRuler is an interface that adds a ProposeRule method.
+// ProposeRuler is the interface that wraps the ProposeRule method.
+//
 // This allows implementors to specify how new blocks are created.
 type ProposeRuler interface {
 	// ProposeRule creates a new proposal.
 	ProposeRule(view hotstuff.View, highQC hotstuff.QuorumCert, cert hotstuff.SyncInfo, cmd *clientpb.Batch) (hotstuff.ProposeMsg, bool)
 }
 
-// Ruleset is the minimum interface that a Hotstuff variant implements.
-// It combines VoteRuler and CommitRuler since they are mandatory. Some variants
-// may implement ProposeRuler.
+// Ruleset is the interface that groups the VoteRule, CommitRule, ProposeRule, and ChainLength methods.
+//
+// Ruleset is the minimum interface that a consensus protocol must implement and defines the
+// rules for voting, committing, proposing, and the chain length required for committing blocks.
 type Ruleset interface {
 	VoteRuler
 	CommitRuler
