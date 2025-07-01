ee9cf469f5f0494734ec31ae3f717f548e0dde4f
Hein Meling <hein.meling@uis.no>
2025-07-01 16:31:49 +0200

    feat: add support for processing cue-based param sweeps directly
    This enhances NewCueExperiments to also handle parameter sweeps
    specified in cue format; see sweep-experiments.cue for an example.
    We still can pass an array of parameter sets as well.

--- internal/config/cue.go
+++ internal/config/cue.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	_ "embed"
+	"errors"
+	"fmt"
 	"os"
 
 	"cuelang.org/go/cue"
@@ -42,4 +44,56 @@ func NewCue(filename string, base *ExperimentConfig) (*ExperimentConfig, error) 
 		return nil, err
 	}
 	return base, nil
+}
+
+func NewCueExperiments(filename string) ([]*ExperimentConfig, error) {
+	ctx := cuecontext.New()
+	schema := ctx.CompileString(schemaFile).LookupPath(cue.ParsePath("config"))
+	if schema.Err() != nil {
+		return nil, schema.Err()
+	}
+	// read and compile the experiments file (top-level array)
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	experimentList := ctx.CompileString(string(b), cue.Filename(filename))
+	if experimentList.Err() != nil {
+		return nil, experimentList.Err()
+	}
+	var head cue.Value
+	var errMissing error
+	// lookup the experiments in the compiled file
+	expandList := experimentList.LookupPath(cue.ParsePath("config.experiments"))
+	if expandList.Err() != nil {
+		errMissing = expandList.Err() // missing config.experiments
+		head = experimentList.Value() // file may contain a list of experiments
+	} else {
+		head = expandList.Value() // file may expanded into a list of experiments
+	}
+
+	// walk the list
+	iter, err := head.List()
+	if err != nil {
+		return nil, fmt.Errorf("experiments file must be or expand into an array: %w", errors.Join(errMissing, err))
+	}
+	var out []*ExperimentConfig
+	for iter.Next() {
+		elem := iter.Value() // { config: { … } }
+		cfg := elem.LookupPath(cue.ParsePath("config"))
+		if cfg.Err() != nil {
+			return nil, cfg.Err()
+		}
+		unified := schema.Unify(cfg)
+		if err := unified.Validate(cue.Concrete(true)); err != nil {
+			return nil, err
+		}
+		var ec ExperimentConfig
+		if err := unified.Decode(&ec); err != nil {
+			return nil, err
+		}
+		out = append(out, &ec)
+	}
+
+	return out, nil
 }
