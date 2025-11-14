9bfaf90cc36a0f7c9f67b801e5e72a7a737725cc
Hein Meling <hein.meling@uis.no>
2025-11-14 23:41:50 +0100

    refactor: move VerifySyncInfo from TimeoutRuler to cert.Authority
    This moves the VerifySyncInfo logic from the simple and aggregate
    timeout rulers into a common logic on the cert.Authority struct,
    along with many other crypto verification methods (also used by
    the VerifySyncInfo method). This also removes the method from the
    TimeoutRuler interface, reducing the interface to only two methods.

--- protocol/synchronizer/timeoutrule_simple.go
+++ protocol/synchronizer/timeoutrule_simple.go
@@ -46,25 +46,4 @@ func (s *Simple) RemoteTimeoutRule(_, timeoutView hotstuff.View, timeouts []hots
 	return hotstuff.NewSyncInfoWith(tc), nil
 }
 
-func (s *Simple) VerifySyncInfo(syncInfo hotstuff.SyncInfo) (qc *hotstuff.QuorumCert, view hotstuff.View, timeout bool, err error) {
-	if timeoutCert, haveTC := syncInfo.TC(); haveTC {
-		if err := s.auth.VerifyTimeoutCert(timeoutCert); err != nil {
-			return nil, 0, timeout, fmt.Errorf("failed to verify timeout certificate: %w", err)
-		}
-		view = timeoutCert.View()
-		timeout = true
-	}
-
-	if quorumCert, haveQC := syncInfo.QC(); haveQC {
-		if err := s.auth.VerifyQuorumCert(quorumCert); err != nil {
-			return nil, 0, timeout, fmt.Errorf("failed to verify quorum certificate: %w", err)
-		}
-		// if there is both a TC and a QC, we use the QC if its view is greater or equal to the TC.
-		if quorumCert.View() >= view {
-			view = quorumCert.View()
-			timeout = false
-		}
-		return &quorumCert, view, timeout, nil
-	}
-	return nil, view, timeout, nil // quorum certificate not present, so no high QC available
-}
+var _ TimeoutRuler = (*Simple)(nil)
