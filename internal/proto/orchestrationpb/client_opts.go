96135d7cdec1e7124477594aa93b8cf2470043f6
Hein Meling <hein.meling@uis.no>
2025-11-15 20:44:02 +0100

    feat: add ClientIDs() []client.ID and ClientIDString() in ClientOpts
    These simplifies usage in the orchestration worker.

--- internal/proto/orchestrationpb/client_opts.go
+++ internal/proto/orchestrationpb/client_opts.go
@@ -1,10 +1,26 @@
 package orchestrationpb
 
 import (
+	"strconv"
+	"unsafe"
+
 	"github.com/relab/hotstuff/client"
 )
 
 // ClientID returns the client's ID.
 func (x *ClientOpts) ClientID() client.ID {
 	return client.ID(x.GetID())
+}
+
+// ClientIDString returns the client's ID as a string.
+func (x *ClientOpts) ClientIDString() string {
+	return strconv.Itoa(int(x.GetID()))
+}
+
+// ClientIDs returns the list of client IDs.
+func (x *StopClientRequest) ClientIDs() []client.ID {
+	if x == nil || len(x.IDs) == 0 {
+		return nil
+	}
+	return unsafe.Slice((*client.ID)(unsafe.Pointer(unsafe.SliceData(x.IDs))), len(x.IDs))
 }
